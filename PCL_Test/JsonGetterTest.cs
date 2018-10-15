2308bc3fde1cd71d91ea1cbad96576aed3ee7bb9
Михаил Новиков <novikovmailg@gmail.com>
2018-10-15 13:24:07 +0300

    111

--- PCL_Test/JsonGetterTest.cs
+++ PCL_Test/JsonGetterTest.cs
@@ -18,10 +18,9 @@ namespace PCL_Test
         {
             _jsonGetter = new JsonGetter();
 
-            var expect = "[{\"firstname\": \"Ivan\"," +
-                "\"lastname\": \"Ivanov\"},{\"firstname\": \"Petr\",\"lastname\": \"Petrov\"}]";
+            var expect = "[\r\n  {\r\n    firstname\": \"Ivan\",\r\n    \"lastname\": \"Ivanov\"\r\n  }\r\n]";
 
-            var actual = _jsonGetter.GetJsonStr("PCL.jsonSmall.json");
+            var actual = _jsonGetter.GetJsonStr(@"PCL.jsonSmall.json");
 
             Assert.AreEqual(expect, actual);
         }
